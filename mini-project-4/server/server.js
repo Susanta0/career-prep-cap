1904c592a8e1da15dbf4ec8a8bcb811b86d439d6
Susanta0 <susanta721467@gmail.com>
2025-06-15 18:09:25 +0530

    updated

--- mini-project-4/server/server.js
+++ mini-project-4/server/server.js
@@ -3,25 +3,28 @@ const cors = require("cors");
 const { dbConnect } = require("./config/db");
 const { authRouter } = require("./routes/auth.routes");
 const { notesRouter } = require("./routes/notes.routes");
-const app = express();
 require("dotenv").config();
+const app = express();
 
 app.use(
   cors({
     origin: ["https://notesapp-flax-pi.vercel.app", "http://localhost:5173"],
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
+app.options("*", cors());
 app.use(express.json());
-
-app.use("/api", authRouter);
-app.use("/api", notesRouter);
 
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "Wellcome my backend" });
 });
+
+app.use("/api", authRouter);
+app.use("/api", notesRouter);
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
